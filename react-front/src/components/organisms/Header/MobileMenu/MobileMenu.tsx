650530e4f259d17fe013b4db8ba1b631b78e770d
Shoxsz <paulomarcio18p1@gmail.com>
2020-04-29 17:19:35 -0300

    favicon and mobile header

--- react-front/src/components/organisms/Header/MobileMenu/MobileMenu.tsx
+++ react-front/src/components/organisms/Header/MobileMenu/MobileMenu.tsx
@@ -5,13 +5,19 @@ import './MobileMenu.sass'
 import SocialList from '../SocialList'
 import CircularImage from '../../../atoms/CircularImage/CircularImage'
 import HeaderList from '../HeaderList'
+import { Link } from 'react-router-dom'
 
 export default class MobileMenu extends React.Component<{}>{
   render(){
     return (
       <Div className="mobile-menu">
-        <Div mb="16px" display="flex">
-          <CircularImage divColor="white" width="96px" height="96px" image="/minotaur.png" />
+        <Div display="flex" alignItems="center">
+          <Div mr="8px" display="flex">
+            <CircularImage divColor="white" width="96px" height="96px" image="/minotaur.png" />
+          </Div>
+          <Div className="mobile-menu__title">
+            <Link to="/"><h2>CRACK THA GAME DEV</h2></Link>
+          </Div>
         </Div>
         <Div mb="64px">
           <SocialList direction="horizontal" itemSize="42px" />
