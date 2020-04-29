e190514a8fbad6668889cd25b2d6dbfae2255334
Shoxsz <paulomarcio18p1@gmail.com>
2020-04-29 09:54:48 -0300

    update ghost key

--- backend/src/services/impl/ghost_configs.ts
+++ backend/src/services/impl/ghost_configs.ts
@@ -1,4 +1,4 @@
-export const GHOST_KEY = "d0ee566657aea8c9285df2dfc5"
+export const GHOST_KEY = "cc47bfc7966609c347583c2211"
 export const API_URL = "http://localhost:2368/ghost/api/v3/"
 
 export const getGhostUrl = function(params : string, query?: any){
