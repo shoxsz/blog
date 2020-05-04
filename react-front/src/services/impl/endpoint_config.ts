d004ce12dac81770935ba24160a27f9a0b16a15a
Shoxsz <paulomarcio18p1@gmail.com>
2020-05-04 09:14:31 -0300

    altered live endpoint to https and domain

--- react-front/src/services/impl/endpoint_config.ts
+++ react-front/src/services/impl/endpoint_config.ts
@@ -1,5 +1,5 @@
 const LOCAL_ADDRESS = "http://192.168.2.103:3030"
-const SERVER_ADDRESS = "http://159.203.88.127/backend"
+const SERVER_ADDRESS = "https://crackthagamedev.com/backend"
 
 export const getEndPointURL = function(){
   if(process.env.NODE_ENV === "production"){
