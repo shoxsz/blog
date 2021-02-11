820542c02ef348f8a1085c1e6c8c2af7051a0dd2
Shoxsz <paulomarcio18p1@gmail.com>
2021-02-11 15:50:03 -0300

    copy

--- react-front/src/components/pages/Ebook/EbookPage.tsx
+++ react-front/src/components/pages/Ebook/EbookPage.tsx
@@ -32,13 +32,13 @@ const EbookPage = function(props){
             </p>
 
             <p>
-            <span className="ebook-text-highlight">O que este eBook é:</span> Uma série de projetos práticos para você usar e aprender, o eBook vai dissecar cada projeto e explicar o que cada função faz, como usar e pra que você deve usar cada função.
-              Cada projeto te ensina um tópico importante da OpenGL, ao final do livro você será capaz de programar aplicações OpenGL sozinho, pois terá um conhecimento muito mais amplo da API.
+            <span className="ebook-text-highlight">O que este eBook é:</span> Uma série de projetos práticos! o eBook vai dissecar cada projeto e explicar o que cada função faz, como usar e para que você deve usar cada função.
+              Em cada projeto você vai ampliar o seu conhecimento sobre OpenGL!
             </p>
             
             <p>
               Para quem está começando este pode ser um conteúdo difícil de encontrar, e quando encontra é difícil de entender, a maioria dos tutoriais ensinam apenas funções antigas da OpenGL,
-              que não são mais usadas mais hoje em dia, além de serem limitados em conteúdo. Este eBook é guia completo para te tirar do zero e te colocar no lugar de um programador OpenGL.
+              que não são mais usadas hoje em dia, além de serem limitados em conteúdo. Esse eBook é guia prático e claro que vai te ensinar definitivamente OpenGL.
             </p>
 
             <p>
@@ -47,22 +47,21 @@ const EbookPage = function(props){
             </p>
 
             <p>
-              <span className="ebook-text-highlight">O que você vai aprender:</span> Em cada capítulo o livro ensina um tópico importante da OpenGL.
+              <span className="ebook-text-highlight">O que você vai aprender:</span>
               <ul className="ebook-chapters">
-                <li><span>No primeiro capítulo: </span>Você vai aprender o famoso Hello World, como criar uma simples aplicação OpenGL com uma janela colorida!</li>
-                <li><span>No segundo capítulo: </span>Você vai aprender a renderizar um simples triângulo na tela, este capítulo está recheado de informações importantes! como programar e criar um shader, como usar a memória de vídeo para carregar dados de geometria, como invocar um shader para renderização, o que são arrays de vértices, layout de vértice, etc.</li>
-                <li><span>No terceiro capítulo: </span>Você vai aprender uma técnica de renderização utilizando índices, essa técnica evita o consumo exagerado de memória e evita a repetição de vértices de um modelo 2D/3D.</li>
-                <li><span>No quarto e quinto capítulos: </span>Você vai aprender a aplicar texturas nas formas geométricas, e como lidar com texturas dentro dos shaders, também verá como utilizar múltiplas texturas e aplicar blend nelas!</li>
-                <li><span>No sexto capítulo: </span>Você vai aprender a criar e aplicar transformações geométricas e como aplica-las no vertex shader.</li>
-                <li><span>No sétimo capítulo: </span>Você vai aprender a criar e aplicar projeções em perspectiva, este tipo de projeção torna nossas aplicações 3D mais realistas! também verá como aplica-las dentro d vertex shader.</li>
-                <li><span>No oitavo e último capítulo: </span>Você vai aprender a técnica de instancing, para renderizar o mesmo modelo 3D diversas vezes, mudando suas propriedades, essa técnica é importante pois pode melhorar, e muito,
-              o desempenho de uma aplicação 3D.</li>
+                <li><span>No primeiro capítulo: </span>Como criar uma simples janela colorida!</li>
+                <li><span>No segundo capítulo: </span>Renderizar um simples triângulo, criar o seu primeiro shader, entender o que são vértices, arrays de vértices, layout de vértices, etc.</li>
+                <li><span>No terceiro capítulo: </span>Usar índices para renderização.</li>
+                <li><span>No quarto e quinto capítulos: </span>Como trabalhar com múltiplas texturas na sua aplicação e nos shaders.</li>
+                <li><span>No sexto capítulo: </span>Criar transformações geométricas e como aplica-las no vertex shader.</li>
+                <li><span>No sétimo capítulo: </span>Criar projeções em perspectiva para deixar as cenas 3D mais realistas.</li>
+                <li><span>No oitavo e último capítulo: </span>Técnica de instancing, como renderizar várias instâncias de um modelo 3D numa única chamada, alterando as suas propriedades.</li>
               </ul>
             </p>
 
             <p>
-            <span className="ebook-text-highlight">Bonus:</span> <span className="ebook-highlight-dark">Um eBook extra gratuito!</span> que vai te ensinar a criar e programar um jogo 2D do zero!
-            aprenda os conceitos e as técnicas utilizadas para renderização de games 2D, como criar um Sprite Batch, um renderizador de fontes 2D, e mais! Para você adquirir esse eBook basta me enviar um email com um pedido, uma semana após adquirir finalizar a sua compra!
+            Adquira o eBook <span className="ebook-highlight-dark">OpenGL na Prática</span> e ganhe o eBook <span className="ebook-text-highlight">Programando um Jogo 2D com OpenGL</span>, que vai te ensinar a criar
+            e organizar um jogo 2D usando OpenGL e C++! é muto fácil, basta me enviar um email(<span className="ebook-highlight-dark">paulomarcio18p1@gmail.com</span>) pedindo o eBook uma semana após adquirir a sua cópia do <span className="ebook-highlight-dark">OpenGL na Prática</span>.
             </p>
 
             <Div display="flex" justifyContent="flex-end"><img width="64" src="ek.png"/></Div>
